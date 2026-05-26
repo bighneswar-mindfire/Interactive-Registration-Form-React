80cba317214a7ec9f35490af4b8d897ff354dca2
bighneswar-mindfire <bighneswarb@mindfiresolutions.com>
2026-05-26 15:05:50 +0530

    chore: initial project configuration

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: [
+      'dist',
+      'node_modules',
+      'commitlint.config.js',
+      'eslint.config.js',
+    ],
+  },
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-unused-vars': 'warn',
+      'prefer-const': 'error',
+    },
+  },
+
+  prettierConfig
+);
